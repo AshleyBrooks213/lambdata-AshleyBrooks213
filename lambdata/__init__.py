54357a3236465ff69dcc40ec6c3d7efce0a91661
Ashley Brooks <ashesval@gmail.com>
2020-12-01 09:32:22 -0600

    moved functions, added print statement

--- lambdata/__init__.py
+++ lambdata/__init__.py
@@ -3,16 +3,7 @@
 import pandas as pd 
 import numpy as np 
 
-def df_cleaner(df):
-    """Clean a df of nulls"""
-    #TODO - implement
-    df = df.dropna()
 
-    return df 
-
-def null_count(df):
-    """Returns the sum of missing values"""
-    return df.isnull().sum()
 
 
 
--- lambdata/example_module.py
+++ lambdata/example_module.py
@@ -1,1 +1,23 @@
+"""Lambdata - a collection of Data Science helper functions"""
+
+import pandas as pd 
+import numpy as np 
+
+def df_cleaner(df):
+    """Clean a df of nulls"""
+    #TODO - implement
+
+    return df.dropna()
+
+def null_count(df):
+    """Returns the sum of missing values"""
+    return df.isnull().sum()
+
+
+
+
+
 FAVORITE_ANIMALS = ['dolphin', 'whale', 'seadragon', 'wolf', 'tiger']
+
+
+print("it worked!")
