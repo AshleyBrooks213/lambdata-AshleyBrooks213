91ee3348df7d4b76668304a768b98f9ccf48710b
Ashley Brooks <ashesval@gmail.com>
2020-12-04 11:06:59 -0600

    made corrections to acme

--- lambdata/acme.py
+++ lambdata/acme.py
@@ -1,23 +1,20 @@
 """A collection of helper functions for Acme Corporation"""
 import random
-import unittest
+#import unittest
 
 
 class Product:
-    def __init__(
-        self,
-        name,
-        price=10,
-        weight=20, 
-        flammability=0.5, 
-        identifier=random.randint(1000000, 999999, 1)):
+    def __init__(self, name, price=10, weight=20, flammability=0.5, identifier):
         """Constructor for Product class"""
         self.name = str(name)
         self.price = int(price)
         self.weight = int(weight)
         self.flammability = float(flammability)
-        self.identifier = int(identifier)
+        self.identifier = random.randit(1000000, 9999999)
 
 
-    
+print("It worked!!")
+
+#if __name__ == "__main__":
+   
 
