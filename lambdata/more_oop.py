52e07376c72811fa806b948d90a6a20c4a21245e
Ashley Brooks <ashesval@gmail.com>
2021-01-12 17:01:39 -0600

    created more_oop.py file

---
+++ lambdata/more_oop.py
@@ -0,0 +1,13 @@
+"""Practicing OOP"""
+import pandas as pd 
+import numpy as np 
+
+
+class Car:
+    def __init__(self, color, mileage):
+        self.color = color
+        self.mileage = mileage
+
+
+    def __str__(self):
+        return f"The {self.color} car has {self.mileage} miles."
