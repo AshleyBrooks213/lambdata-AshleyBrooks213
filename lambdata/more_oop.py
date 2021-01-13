b9ff16072421632e488c254d299eacec2eb1bdb9
Ashley Brooks <ashesval@gmail.com>
2021-01-12 21:39:19 -0600

    finished more_oop file

--- lambdata/more_oop.py
+++ lambdata/more_oop.py
@@ -11,3 +11,40 @@ class Car:
 
     def __str__(self):
         return f"The {self.color} car has {self.mileage} miles."
+
+
+class Dog:
+    species = "Canis familiaris"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
+
+
+    def speak(self, sound):
+        return f"{self.name} barks: {sound}"
+
+
+class JackRussellTerrier(Dog):
+    def speak(self, sound="Arf"):
+        return super().speak(sound)
+
+
+class Dachshund(Dog):
+    pass
+
+
+class Bulldog(Dog):
+    pass
+
+
+if __name__ == "__main__":
+
+    miles = Dog("Miles", 4)
+    print(miles)
+
+
